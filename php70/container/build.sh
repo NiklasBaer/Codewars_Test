6fd1f9053263f75eee7a8c16b59500604f5c7ccb
Andre Rademacher <andre.rademacher@check24.de>
2023-02-19 15:49:07 +0100

    added xdebug-2.7.2

--- php70/container/build.sh
+++ php70/container/build.sh
@@ -4,6 +4,7 @@ SCRIPT_DIRECTORY="$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")"
 cd "${SCRIPT_DIRECTORY}" || exit
 
 docker build \
+  --no-cache\
   --pull \
   --tag "andrerademacher/codewars-php70" \
   .
